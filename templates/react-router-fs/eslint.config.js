f93403443064d3b9819f66d4892cf99d58685514
ringuralte <ringuralte@gmail.com>
2025-10-23 16:14:03 +0530

    feat(wip): react-router-fs template

---
+++ templates/react-router-fs/eslint.config.js
@@ -0,0 +1,13 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    formatters: true,
+    react: true,
+  },
+  {
+    rules: {
+      'eslint-comments/no-unlimited-disable': 'off',
+    },
+  },
+)
