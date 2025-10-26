4bbe4f457c5549ef9fdad8d41af920943989f188
ringuralte <ringuralte@gmail.com>
2025-10-26 14:09:30 +0530

    moving routing to features folder

---
+++ templates/features/ui/tailwind/eslint.config.js
@@ -0,0 +1,24 @@
+import antfu from '@antfu/eslint-config'
+import betterTailwind from 'eslint-plugin-better-tailwindcss'
+
+export default antfu(
+  {
+    formatters: true,
+    react: true,
+  },
+  {
+    plugins: {
+      'better-tailwindcss': betterTailwind,
+    },
+    rules: {
+      ...betterTailwind.configs.recommended.rules,
+      'eslint-comments/no-unlimited-disable': 'off',
+      'better-tailwindcss/no-unregistered-classes': 'off',
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'app/app.css',
+      },
+    },
+  },
+)
